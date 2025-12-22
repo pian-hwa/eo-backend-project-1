62f0245cd29d15724ba2dc951920255e75b19300
HuiSeong Kim <pianhwa0913@gmail.com>
2025-12-22 23:49:41 +0900

    [깃헙 페이지 등록 테스트] 구글 계정 로그인 버튼 활성화

--- workspace/js/oauth.js
+++ workspace/js/oauth.js
@@ -3,8 +3,12 @@ const client = window.supabase.createClient(
     "sb_publishable_qy3FLPFdwpoAb0my107Vjw_ndi60_3Z"
 );
 
+const googleLogin = document.querySelector("#login > form > .socials > .google");
+
 async function loginWithGoogle() {
     await client.auth.signInWithOAuth({
         provider: "google"
     });
 }
+
+googleLogin.addEventListener("click", loginWithGoogle());
